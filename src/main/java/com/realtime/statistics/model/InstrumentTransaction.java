f4dd959a6ae25dc9b69444319a0a30ef515216a8
91860 <lalitkulkarniofficial@gmail.com>
2020-09-25 12:31:58 +0530

    Added validation on negative price

--- src/main/java/com/realtime/statistics/model/InstrumentTransaction.java
+++ src/main/java/com/realtime/statistics/model/InstrumentTransaction.java
@@ -3,6 +3,8 @@ package com.realtime.statistics.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
@@ -13,6 +15,7 @@ public class InstrumentTransaction {
     @NotNull(message = "Instrument is a mandatory field")
     private String instrument;
 
+    @Min(value = 0,message = "Price cannot be negative")
     private double price;
 
     private long timestamp;
